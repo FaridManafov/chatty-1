81b2868ca3d106c505c99a55fdc737fcc2fdf10d
jonathandannel <usixjad@gmail.com>
2018-07-10 20:40:37 +0000

    got rid of separate data.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import NavBar from './NavBar.jsx'
 import MessageList from './MessageList.jsx'
 import ChatBar from './ChatBar.jsx'
-// import messages from './data.jsx';
 
 export default class App extends Component {
   state = {
@@ -11,25 +10,25 @@ export default class App extends Component {
     },
     messages: [
       {
-        id: 1,
+        id: 111,
         type: "incomingMessage",
         content: "I won't be impressed with technology until I can download food.",
         username: "Anonymous1"
       },
       {
-        id: 2,
+        id: 222,
         type: "incomingMessage",
         content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
         username: "Anonymous2"
       },
       {
-        id: 3,
+        id: 333,
         type: "incomingMessage",
         content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
         username: "Anonymous2"
       },
       {
-        id: 4,
+        id: 444,
         type: "incomingMessage",
         content: "This isn't funny. You're not funny",
         username: "nomnom"
@@ -37,11 +36,24 @@ export default class App extends Component {
     ]
   }
 
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        <MessageList messages={this.state.messages}/>
+        <MessageList messages={this.state.messages} />
         <ChatBar currentUser={this.state.currentUser.name} />
       </div>
     );
