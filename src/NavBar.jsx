cc1c7fe4fbba7dafe3433a0aeaf08fc4ff0952ab
jonathandannel <usixjad@gmail.com>
2018-07-13 00:18:15 +0000

    added some minor styling

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default function NavBar(props) {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">chatty.io</a>
+        <a href="/" className="navbar-brand">chatty</a>
           <span>Users Online: {props.totalUsers}</span>
       </nav>
     )
